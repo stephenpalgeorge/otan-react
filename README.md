b0ca2089cf3559309d15895ae71df17328b1ac86
Stephen Pal-George <stephenngeorge@gmail.com>
2019-12-01 14:47:43 +0000

    prepare for global install

---
+++ README.md
@@ -0,0 +1,6 @@
+# LORIS REACT
+
+An opinionated CLI for scaffolding react components and a react component library.
+Please come back soon, I'm working on writing this file up properly with some 
+actually useful information
+
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const fs = require('fs')
-const chalk = require('chalk')
+const setupLibrary = require('./lib/setup-library')
+const generateComponent = require('./lib/generate-component')
 
-const addScripts = () => {
-    const rootDir = process.cwd()
-    if (fs.existsSync(`${rootDir}/package.json`)) {
-        const pkg = require(`${rootDir}/package.json`)
-        pkg.scripts["test"] = "testing"
-        pkg.scripts["example"] = "another script"
-        const package = JSON.stringify(pkg)
-    }
-    else {
-        console.warn(
-            chalk.yellow('we couldn\'t find a package.json file...')
-        )
-    }
+module.exports = {
+    setupLibrary, generateComponent
 }
-
-addScripts()
