b32f3adf317720bdcf3e3973bff92856152e761f
Stephen Pal-George <stephenngeorge@gmail.com>
2019-12-01 15:52:55 +0000

    add comments to all files

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
 const generateComponent = require('./lib/generate-component')
+const setupLibrary = require('./lib/setup-library')
 
 let args = process.argv.splice(2)
 
-if (args[0] == "setup") require('./lib/setup-library')
+switch(args[0]) {
+    case "setup":
+        setupLibrary()
+        break
+    case "new":
+        generateComponent()
+        break
+    default:
+        setupLibrary()
+}
 
 module.exports = {
     setupLibrary, generateComponent
