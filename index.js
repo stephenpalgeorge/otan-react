3a917a06dd55e25a1a8a69d834444a2f5441a0eb
Stephen Pal-George <stephenngeorge@gmail.com>
2019-12-01 15:05:07 +0000

    global import bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const setupLibrary = require('./lib/setup-library')
+#!/usr/bin/env node
+
 const generateComponent = require('./lib/generate-component')
+
+let args = process.argv.splice(2)
+
+if (args[0] == "setup") require('./lib/setup-library')
 
 module.exports = {
     setupLibrary, generateComponent
